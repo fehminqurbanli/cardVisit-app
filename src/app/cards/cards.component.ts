8f63e0e181a1fef978008aa1cf715f3416d850ad
fahmin.q <fehmi@DESKTOP-6P9HRUK>
2023-09-04 10:55:42 +0400

    04/09/2023

---
+++ src/app/cards/cards.component.ts
@@ -0,0 +1,20 @@
+import { Component,OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrls: ['./cards.component.scss']
+})
+export class CardsComponent implements OnInit {
+
+  cardItem={
+    name:'Fahmin',
+    phone:'String'
+  };
+
+constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+}
